6814476efc733693a24b7933fb6c5a5f9a7a3f79
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-07-17 17:05:17 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2 as cv
 
-vid = cv.VideoCapture("videos/video.mp4")
+capture = cv.VideoCapture("videos/video.mp4")
 
 while True:
-    success, img = vid.read()
+    success, img = capture.read()
     cv.imshow("video", img)
 
     if cv.waitKey(24) == ord('q'):
         break
+
+capture.release()
+cv.destroyAllWindows()
