9f6a764b1c0587565715c84eb0b63cd61beed97f
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-07-18 20:35:39 +0100

    made a lot of things

--- main.py
+++ main.py
@@ -20,23 +20,22 @@ while True:
     # selecting range of green color using HSV
     lowerHue = cv.getTrackbarPos("lower_hue", "controls")
     upperHue = cv.getTrackbarPos("upper_hue", "controls")
-    lower_blue = np.array([lowerHue,50,75])
+    lower_blue = np.array([lowerHue,50,50])
     upper_blue = np.array([upperHue,255,255])
     # making binary image as a mask using the hsv color range
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     if len(contours) != 0:
-        # find the biggest countour (c) by the area
+        # find the biggest countour
         biggestConture = max(contours, key = cv.contourArea)
-        cv.drawContours(frame, [biggestConture], -1, (0, 0, 255), -1)
-        # reduce the ammount of points to know the shapes
+        # reduce the ammount of points to know the shape
         peri = cv.arcLength(biggestConture, True)
         approx = cv.approxPolyDP(biggestConture, 0.04 * peri, True)
-        print(len(approx))
-        # draw rectangle on the biggest contour (c) in green
+        # selecting rectangle on the biggest contour
         x,y,w,h = cv.boundingRect(biggestConture)
-        cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+        cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0) if len(approx) == 3 else (0,0,255),2)
+        cv.putText(frame, str(len(approx)) + " points", (x, y - 20), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), thickness=2)
 
     cv.imshow("frame", frame)
     cv.imshow("mask", mask)
