6f90e126c2be8a8a346896dd720b80c3d2cc9b4f
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-07-18 15:59:58 +0100

    made opencv add a rectangle on the biggest green conture in the frame

--- main.py
+++ main.py
@@ -3,36 +3,41 @@ import cv2  as cv
 
 cap = cv.VideoCapture(0)
 
-lowerHue = 0
-upperHue = 0
-
+# controls
 def nothing(v):
     pass
-
 cv.namedWindow("controls")
-cv.createTrackbar("lower_hue", "controls", 0, 255, nothing)
-cv.createTrackbar("upper_hue", "controls", 0, 255, nothing)
+cv.createTrackbar("lower_hue", "controls", 56, 255, nothing)
+cv.createTrackbar("upper_hue", "controls", 78, 255, nothing)
 
+# video stream while loop
 while True:
     _, frame = cap.read()
-    # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    # define range of blue color in HSV
-    lower_blue = np.array([cv.getTrackbarPos("lower_hue", "controls"),50,50])
-    upper_blue = np.array([cv.getTrackbarPos("upper_hue", "controls"),255,255])
-    # Threshold the HSV image to get only blue colors
+    # Converting BGR to HSV for better color selecting
+    hsv = cv.cvtColor(cv.blur(frame, (15, 15)), cv.COLOR_BGR2HSV)
+    # selecting range of green color using HSV
+    lowerHue = cv.getTrackbarPos("lower_hue", "controls")
+    upperHue = cv.getTrackbarPos("upper_hue", "controls")
+    lower_blue = np.array([lowerHue,50,75])
+    upper_blue = np.array([upperHue,255,255])
+    # making binary image as a mask using the hsv color range
     mask = cv.inRange(hsv, lower_blue, upper_blue)
-    bluecnts = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[-2]
+    contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
-    if len(bluecnts)>0:
-        blue_area = max(bluecnts, key=cv.contourArea)
-        (xg,yg,wg,hg) = cv.boundingRect(blue_area)
-        cv.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
+    if len(contours) != 0:
+        cv.drawContours(frame, contours, -1, 255, 3)
+        # find the biggest countour (c) by the area
+        biggestConture = max(contours, key = cv.contourArea)
+        x,y,w,h = cv.boundingRect(biggestConture)
+        # draw the biggest contour (c) in green
+        cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
-    cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
+    cv.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
-    if cv.waitKey(5) == 27:
+    cv.imshow("frame", frame)
+    cv.imshow("mask", mask)
+
+    if cv.waitKey(5) == 113:
         break
 
 cap.release()
